fbcc95f7c1a1aef96853ab1d7153ff157e2b0c35
Noah Zalev <github@nzalev.ca>
2022-06-29 20:10:28 -0400

    improved db insertion

--- parser.py
+++ parser.py
@@ -10,32 +10,38 @@ def parse_line(line: str) -> tuple:
     return (a_id, byte_len, bytes_str)
 
 
-def insert_line(coll, line: tuple) -> None:
+def insert_line(line: tuple, counter: int, frames: list) -> None:
     message = {
         'arbitration_id': line[0],
         'data_len': line[1],
-        'data_string': line[2]
+        'data_string': line[2],
+        'counter': counter
     }
 
-    coll.insert_one(message)
+    frames.append(message)
 
 
 def process_file(filename: str, collection) -> None:
     with open(filename) as f:
         data = f.readlines()
 
+    frames = []
+    counter = 0
     for line in data:
         l = line.strip('\n')
-        insert_line(collection, parse_line(l))
+        insert_line(parse_line(l), counter, frames)
+        counter += 1
+
+    collection.insert_many(frames)
 
 
 
 client = MongoClient('mongodb://127.0.0.1:27017')
 db = client.canmsgs
-can_a = db.can_a
-can_b = db.can_b
+can_a = db.idlebattery
 
-process_file('cana.txt', can_a)
-process_file('canb.txt', can_b)
+
+process_file('1731.txt', can_a)
+
 
 
